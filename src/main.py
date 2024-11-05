18d7d832699dce61a84c23aeda9d1fc18ef8c4e6
TheDogHusky <contact@classydev.fr>
2024-11-05 09:16:38 +0100

    feat: initial commit

---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello, World!")
+
+# Entourer le programme dans une fonction main avec cette condition permet d'éviter l'exécution du code si on importe le fichier. C'est une convention.
+if __name__ == "__main__":
+    main()
