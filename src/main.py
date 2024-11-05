925a1ded17db3597e52660e49e3bacbecda232ce
TheDogHusky <contact@classydev.fr>
2024-11-05 09:20:33 +0100

    feat: ajout d'un return dans le main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 def main():
     print("Hello, World!")
+    return
 
 # Entourer le programme dans une fonction main avec cette condition permet d'éviter l'exécution du code si on importe le fichier. C'est une convention.
 if __name__ == "__main__":
